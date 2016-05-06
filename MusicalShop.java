60ff472c1907d3b4a782cdf50b18efd6a9973ea9
oleszavadskiy <oleszavadskiy@gmail.com>
2016-05-06 14:49:50 +0300

    Added files via upload

---
+++ MusicalShop.java
@@ -0,0 +1,11 @@
+package com.goit.gojavaonline2;
+
+import java.util.List;
+
+/**
+ * Created by Олесь on 12.04.2016.
+ */
+public class MusicalShop  {
+    List<MusicalInstrument> instruments;
+
+}
---
+++ Trumpet.java
@@ -0,0 +1,7 @@
+package com.goit.gojavaonline2;
+
+/**
+ * Created by Олесь on 12.04.2016.
+ */
+public class Trumpet extends MusicalInstrument {
+}
